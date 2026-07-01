47c6e7d93493ca14cdda0287386e254a943f564d
Asher Pirov <Asherpirov@gmail.com>
2026-07-01 15:49:54 +0200

    add vaidationresult, basevalidator classes and ivalidator interface

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using IntelligencePipeline.Models.Enums;
+using IntelligencePipeline.Models.Reports;
 using System;
 namespace IntelligencePipeline;
 
@@ -10,6 +11,9 @@ class Program
 
     public static void Main()
     {
-
+        SoldierReport a = new SoldierReport(1, DateTime.Now, 30.5, 34.5, "Weapon","asher", "8809022","8200",4);
+        Console.WriteLine(a.ToString());
+        Console.WriteLine(a.GetSourceType());
+        Console.WriteLine(a.CalculateReliabilityScore());
     }
 }
--- Validation/BaseValidator..cs
+++ Validation/BaseValidator..cs
@@ -1,8 +1,48 @@
-﻿using System;
-using IntelligencePipeline.Models.Reports;
+﻿using IntelligencePipeline.Models.Reports;
 using IntelligencePipeline.Validation;
+using System;
 
 namespace IntelligencePipeline.Validation;
 
+abstract class BaseValidator : IValidator
+{
+ 
+
+    public abstract ValidationResult Validate(Report report);
+    
+    protected ValidationResult ValidateCommonFields(Report report)
+    {
+        if (report.Timestamp < report.minDate)
+
+        {
+            return ValidationResult.Failure("Report timestamp cannot be before 2020-01-01.");
+        }
+        
+        if (report.Timestamp > DateTime.Now)
+        {
+            return ValidationResult.Failure("Report timestamp cannot be in the future.");
+        }
+
+        if (report.Latitude < Report.MIN_LATITUDE || report.Latitude > Report.MAX_LATITUDE )
+        {
+            return ValidationResult.Failure("Report Latitude must be between 29.5000 - 33.5000");
+        }
+
+        if (report.Longitude < Report.MIN_LONGITUDE || report.Longitude > Report.MAX_LONGITUDE)
+        {
+            return ValidationResult.Failure("Report Longitude must be between 34.0000 - 36.0000");
+        }
+
+        if (report.Description.Length < Report.MIN_CHARS || report.Description.Length > Report.MAX_CHARS)
+        {
+            return ValidationResult.Failure("Report Description must be between 10-500 characters");
+        }
+
+        return ValidationResult.Success();
+    }
+
+    protected abstract ValidationResult ValidateSpecificFields(Report report);
+
+}
 
 
